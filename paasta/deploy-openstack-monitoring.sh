ae28373b63f99265b72c4b8af6fd34e1042e6d5e
Hansu Oh <hso@innogrid.com>
2021-01-27 16:09:47 +0900

    UPDATE MONITORING-DEPLOYMENT 1. paasta

--- paasta/deploy-openstack-monitoring.sh
+++ paasta/deploy-openstack-monitoring.sh
@@ -7,7 +7,6 @@ bosh -e micro-bosh -d paasta -n deploy paasta-deployment.yml \
   -o operations/use-haproxy-public-network.yml \
   -o operations/use-compiled-releases-haproxy-online.yml \
   -o operations/use-postgres.yml \
-  -o operations/use-compiled-releases-postgres-online.yml \
 	-o operations/rename-network-and-deployment.yml \
 	-o paasta-addon/paasta-monitoring.yml \
   -o operations/addons/enable-component-syslog.yml \
