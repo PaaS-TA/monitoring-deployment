29ad006a710dbf5ef733dac49bb5e5c29a8fd59e
hso <51317201+hsohans@users.noreply.github.com>
2021-01-27 16:15:31 +0900

    Update deploy-openstack-monitoring.sh

--- paasta/deploy-openstack-monitoring.sh
+++ paasta/deploy-openstack-monitoring.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 bosh -e micro-bosh -d paasta -n deploy paasta-deployment.yml \
-  -o operations/openstack.yml \
-  -o operations/use-compiled-releases-online.yml \
-  -o operations/use-haproxy.yml \
-  -o operations/use-haproxy-public-network.yml \
-  -o operations/use-compiled-releases-haproxy-online.yml \
-  -o operations/use-postgres.yml \
-  -o operations/rename-network-and-deployment.yml \
-  -o paasta-addon/paasta-monitoring.yml \
-  -o operations/addons/enable-component-syslog.yml \
-  -l openstack-vars.yml \
-  -l ../../common/common_vars.yml
+        -o operations/openstack.yml \
+        -o operations/use-compiled-releases-online.yml \
+        -o operations/use-haproxy.yml \
+        -o operations/use-haproxy-public-network.yml \
+        -o operations/use-compiled-releases-haproxy-online.yml \
+        -o operations/use-postgres.yml \
+        -o operations/rename-network-and-deployment.yml \
+        -o paasta-addon/paasta-monitoring.yml \
+        -o operations/addons/enable-component-syslog.yml \
+        -l openstack-vars.yml \
+        -l ../../common/common_vars.yml
