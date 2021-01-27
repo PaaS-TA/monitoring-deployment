f660cedb96dae64e9a5a51fa11749200c6dff456
hso <51317201+hsohans@users.noreply.github.com>
2021-01-27 16:14:48 +0900

    Update deploy-aws-monitoring.sh

--- paasta/deploy-aws-monitoring.sh
+++ paasta/deploy-aws-monitoring.sh
@@ -9,6 +9,6 @@ bosh -e micro-bosh -d paasta -n deploy paasta-deployment.yml \
 	-o operations/use-postgres.yml \
 	-o operations/rename-network-and-deployment.yml \
 	-o paasta-addon/paasta-monitoring.yml \
-  -o operations/addons/enable-component-syslog.yml \
+  	-o operations/addons/enable-component-syslog.yml \
 	-l aws-vars.yml \
 	-l ../../common/common_vars.yml
