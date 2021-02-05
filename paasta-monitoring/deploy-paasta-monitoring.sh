ce980584f5d3740c3d06b8c96503c451afc3421e
hso <51317201+hsohans@users.noreply.github.com>
2021-02-05 16:02:41 +0900

    Update deploy-paasta-monitoring.sh
    UPDATE monitoring-deployment
    modify deploy-paasta-monitoring.sh

--- paasta-monitoring/deploy-paasta-monitoring.sh
+++ paasta-monitoring/deploy-paasta-monitoring.sh
@@ -3,4 +3,5 @@
 bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml  \
 	-o use-public-network-aws.yml \
 	-l paasta-monitoring-vars.yml \
-	-l ../../common/common_vars.yml
+	-l ../../common/common_vars.yml \
+	-l pem.yml
