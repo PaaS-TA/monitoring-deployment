873f45bf5098ebd8b0134e90160d7c30e9b42f98
Minsoo, Kim <32118423+thouy@users.noreply.github.com>
2021-11-24 19:59:39 +0900

    Update deploy-openstack-monitoring.sh

--- paasta-addon/deploy-openstack-monitoring.sh
+++ paasta-addon/deploy-openstack-monitoring.sh
@@ -7,10 +7,10 @@ bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
         -o operations/use-haproxy.yml \
         -o operations/use-haproxy-public-network.yml \
         -o operations/use-postgres.yml \
-        -o operations/rename-network-and-deployment.yml \
+        -o operations/cce.yml \
+	-o operations/rename-network-and-deployment.yml \
         -o operations/addons/paasta-monitoring-agent.yml \
         -o operations/addons/enable-component-syslog.yml \
 	-o operations/addons/enable-zabbix-agent.yml \
-        -o operations/cce-postgres.yml \
         -l vars.yml \
         -l ../../common/common_vars.yml
