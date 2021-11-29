cc9c706e2cf1435d8c5f924edb3a6563cb6e80b6
YoongJwe <yoongjwe@gmail.com>
2021-11-29 18:07:02 +0900

    Update PaaS-TA deployment script (+ Monitoring Dashboard) for AWS

--- paasta-addon/deploy-aws-monitoring.sh
+++ paasta-addon/deploy-aws-monitoring.sh
@@ -3,14 +3,15 @@
 BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
 
 bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
-	-o operations/aws.yml \
-	-o operations/use-haproxy.yml \
-	-o operations/use-haproxy-public-network.yml \
-	-o operations/use-postgres.yml \
-	-o operations/rename-network-and-deployment.yml \
-	-o operations/addons/paasta-monitoring-agent.yml \
-  	-o operations/addons/enable-component-syslog.yml \
-	-o operations/addons/enable-zabbix-agent.yml \
-	-o operations/cce-postgres.yml \
-	-l vars.yml \
-	-l ../../common/common_vars.yml
+    -o operations/aws.yml \
+    -o operations/use-haproxy.yml \
+    -o operations/use-haproxy-public-network.yml \
+    -o operations/use-postgres.yml \
+    -o operations/cce.yml \
+    -o operations/rename-network-and-deployment.yml \
+    -o operations/addons/paasta-monitoring-agent.yml \
+    -o operations/addons/enable-component-syslog.yml \
+    -o operations/addons/enable-zabbix-agent.yml \
+    -o operations/addons/custom-diego-release-for-monitoring.yml \
+    -l vars.yml \
+    -l ../../common/common_vars.yml
