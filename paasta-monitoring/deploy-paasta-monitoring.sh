fd87a806c1880d48e6c2a4a9b8a074f681d6701a
YoongJwe <78844504+YoongJwe@users.noreply.github.com>
2022-02-28 15:36:32 +0900

    Update deploy-paasta-monitoring.sh

--- paasta-monitoring/deploy-paasta-monitoring.sh
+++ paasta-monitoring/deploy-paasta-monitoring.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml  \
-	-o use-public-network-aws.yml \
-	-o addons/enable-zabbix-agent.yml \
-	-l paasta-monitoring-vars.yml \
-	-l ../../common/common_vars.yml
+# paasta-monitoring-vars.yml 파일에서 IaaS 모니터링 옵션이 사용되는지 확인하고 조건에 따라 iaas-monitoring-batch VM 배포 여부를 분기한다.
+
+ScanIaaSOption=`grep IaaS paasta-monitoring-vars.yml`
+
+if [ -n "$ScanIaaSOption" ]; then
+    bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml \
+        -o use-public-network-aws.yml \
+        -o addons/enable-zabbix-agent.yml \
+        -l paasta-monitoring-vars.yml \
+        -l ../../common/common_vars.yml
+else
+    bosh -e micro-bosh -n -d paasta-monitoring deploy paasta-monitoring.yml \
+        -o use-public-network-aws.yml \
+        -o addons/enable-zabbix-agent.yml \
+        -o addons/disable-iaas-monitoring-batch.yml \
+        -l paasta-monitoring-vars.yml \
+        -l ../../common/common_vars.yml
+fi
