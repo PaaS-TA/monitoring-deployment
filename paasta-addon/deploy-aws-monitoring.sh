4aa562bd1c9d6436f5d72ae9b5064af99aa3a534
YoongJwe <yoongjwe@gmail.com>
2022-04-01 13:53:26 +0900

    Refactor PaaS-TA deployment scripts for AWS/OpenStack

--- paasta-addon/deploy-aws-monitoring.sh
+++ paasta-addon/deploy-aws-monitoring.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"			 # bosh director alias name (PaaS-TA에서 제공되는 create-bosh-login.sh 미 사용시 bosh envs에서 이름을 확인하여 입력)
+# bosh director alias name (paasta-deployment에서 제공되는 create-bosh-login.sh 파일 미사용시 bosh envs에서 이름을 확인하여 직접 입력한다)
+BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"
 
-bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
+bosh -e ${BOSH_ENVIRONMENT} -n -d paasta deploy paasta-deployment.yml \
     -o operations/aws.yml \
     -o operations/use-haproxy.yml \
     -o operations/use-haproxy-public-network.yml \
@@ -11,7 +12,6 @@ bosh -e ${BOSH_ENVIRONMENT} -d paasta -n deploy paasta-deployment.yml \
     -o operations/rename-network-and-deployment.yml \
     -o operations/addons/paasta-monitoring-agent.yml \
     -o operations/addons/enable-component-syslog.yml \
-    -o operations/addons/enable-zabbix-agent.yml \
     -o operations/addons/custom-diego-release-for-monitoring.yml \
     -l vars.yml \
     -l ../../common/common_vars.yml
