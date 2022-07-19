d2177e0d978c6e916656d343f8b19f3111021fe3
YoongJwe <yoongjwe@gmail.com>
2022-07-19 09:00:49 +0900

    Update pinpoint-monitoring directory
    - Modify installation script for no using zabbix-agent when not need IaaS option
    - Remove unused variable name

--- pinpoint-monitoring/deploy-pinpoint-monitoring.sh
+++ pinpoint-monitoring/deploy-pinpoint-monitoring.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo 'y' | bosh -e micro-bosh -d pinpoint-monitoring deploy pinpoint-monitoring.yml \
-	-o use-public-network.yml \
-	-o addons/enable-zabbix-agent.yml \
-	-l pinpoint-monitoring-vars.yml \
-	-l ../../common/common_vars.yml
+# paasta-monitoring-vars.yml 파일에서 IaaS 모니터링 옵션이 사용되는지 확인하고 조건에 따라 zabbix-agent Addon 여부를 분기한다.
+ScanIaaSOption=`grep IaaS ../paasta-monitoring/paasta-monitoring-vars.yml`
+
+if [ -n "$ScanIaaSOption" ]; then
+    bosh -e micro-bosh -n -d pinpoint-monitoring deploy pinpoint-monitoring.yml \
+        -o use-public-network.yml \
+        -o addons/enable-zabbix-agent.yml \
+        -l pinpoint-monitoring-vars.yml \
+        -l ../../common/common_vars.yml
+else
+    bosh -e micro-bosh -n -d pinpoint-monitoring deploy pinpoint-monitoring.yml \
+        -o use-public-network.yml \
+        -l pinpoint-monitoring-vars.yml \
+        -l ../../common/common_vars.yml
+fi
