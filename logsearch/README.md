fc654422a7e29a9426d6510b14830ceadfd0e626
YoongJwe <yoongjwe@gmail.com>
2022-09-26 06:04:48 +0000

    Delete 'logsearch'
    - 'logsearch' 디렉터리 삭제(Deprecated Deployment).

--- logsearch/README.md
+++
@@ -1,3 +0,0 @@
-## Logserch Guide
-
-- [PaaS-TA PaaS Monitoring Logsearch 설치(필수)](https://github.com/PaaS-TA/Guide/blob/working-5.5/service-guide/monitoring/PAAS-TA_MONITORING_LOGSEARCH_INSTALL.md)
--- logsearch/deploy-logsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bosh -e micro-bosh -n -d logsearch deploy logsearch-deployment.yml \
-	-o operations/enable-router.yml \
-	-o addons/enable-zabbix-agent.yml \
-	-l logsearch-vars.yml \
-	-l ../../common/common_vars.yml
